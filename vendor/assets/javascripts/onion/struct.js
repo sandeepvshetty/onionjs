e6963d10dc948672f4f2c0e5666c30880ad4ae40
Mark Evans <mark@new-bamboo.co.uk>
2013-08-02 11:03:34 +0100

    writer decorators work when using setAttrs Conflicts: 	vendor/assets/javascripts/onion/struct.js

--- vendor/assets/javascripts/onion/struct.js
+++ vendor/assets/javascripts/onion/struct.js
@@ -42,7 +42,19 @@ define([
         this[collection]().set(items)
       },
 
+      __writerDecoratorFor__: function (attribute) {
+        var decorators = this.constructor.__writerDecorators__
+        return decorators && decorators[attribute]
+      },
+
       __write__: function(key, value) {
+        var decorator = this.__writerDecoratorFor__(key)
+        if (decorator) {
+          var ignoreDecorator = value === null && decorator.options.includeNull != true
+          if (!ignoreDecorator) {
+            value = decorator.func(value)
+          }
+        }
         if(this.constructor.attributeNames.indexOf(key) != -1){
           this.__writeAttribute__(key, value)
         } else if (this.constructor.collectionNames.indexOf(key) != -1){
@@ -161,10 +173,8 @@ define([
       },
 
       decorateWriter: function (attribute, decorator, options) {
-        var includeNull = !!(options && options.includeNull)
-        this.decorate(setterMethodName(attribute), function (setter, value) {
-          setter((includeNull || value != null) ? decorator(value) : value)
-        })
+        if(!this.__writerDecorators__) this.__writerDecorators__ = {}
+        this.__writerDecorators__[attribute] = {func: decorator, options: (options || {})}
         return this
       },
 
